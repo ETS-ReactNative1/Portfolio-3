dc8dfdb627e7038b83597ea6f069a49ad6b9353a
BuddhaL1T3 <jmriggs75@gmail.com>
2018-09-13 19:58:32 -0400

    fixed project padding and footer icons

--- src/components/footer.js
+++ src/components/footer.js
@@ -12,22 +12,26 @@ class Footer extends Component {
       <footer className="footer" role="contentinfo">
         <h2 id="footer">Get In Touch:</h2>
         <section className="contact-info">
-          <a href="mailto:jmriggs75@gmail.com">
-            <FontAwesomeIcon icon="envelope" size="4x" />
-          </a>
-          <a target="_blank" rel="noopener noreferrer" href="https://github.com/BuddhaL1T3">
-            <FontAwesomeIcon icon={['fab', 'github']} size="4x" />
-          </a>
-          <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/jonathan-riggs-34898476/">
-            <FontAwesomeIcon icon={['fab', 'linkedin']} size="4x" />
-          </a>
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://docs.google.com/document/d/1G7kycQHbakF1DtBzlFg1a_TKWoG_Pihc2yEhLEtEgdI/edit?usp=sharing"
-          >
-            <FontAwesomeIcon icon="file-pdf" size="4x" />
-          </a>
+          <div className="contact-info-left">
+            <a href="mailto:jmriggs75@gmail.com">
+              <FontAwesomeIcon icon="envelope" size="4x" />
+            </a>
+            <a target="_blank" rel="noopener noreferrer" href="https://github.com/BuddhaL1T3">
+              <FontAwesomeIcon icon={['fab', 'github']} size="4x" />
+            </a>
+          </div>
+          <div className="contact-info-right">
+            <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/jonathan-riggs-34898476/">
+              <FontAwesomeIcon icon={['fab', 'linkedin']} size="4x" />
+            </a>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://docs.google.com/document/d/1G7kycQHbakF1DtBzlFg1a_TKWoG_Pihc2yEhLEtEgdI/edit?usp=sharing"
+            >
+              <FontAwesomeIcon icon="file-pdf" size="4x" />
+            </a>
+          </div>
         </section>
       </footer>
     );
